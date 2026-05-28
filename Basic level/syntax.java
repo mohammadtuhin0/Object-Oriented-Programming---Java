7573cd140fceb8557e5e60c396eacff7885a9f58
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-05-28 20:47:55 +0600

    basic

---
+++ Basic level/syntax.java
@@ -0,0 +1,7 @@
+public class syntax{
+    public static void main(String[] argv){
+        System.out.println("Hello world!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning java is fun!");
+    }
+}
