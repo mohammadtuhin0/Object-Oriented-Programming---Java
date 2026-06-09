a972548ef9960a1efd3bffdd32c50141170cf53e
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-06-09 23:49:42 +0600

    Variables in java

---
+++ Basic level/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main() {
+        String name = "Mohammad Tuhin";
+        int age = 21;
+        System.out.println(name);
+        System.out.println(age);
+
+        int x = 5, y = 4;
+        System.err.println(x * y);
+    }
+}
