bc1bd699ea3fbb1c2096a98e443007b458279def
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-06-09 23:30:07 +0600

    Java Statement

---
+++ Basic level/Statement.java
@@ -0,0 +1,7 @@
+public class Statement {
+    public static void main(String[] args) {
+        System.out.println("Java is fun!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning Java is fun!");
+    }
+}
