fc41de78d28951b4acbaa70c233bd755333f3372
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-06-09 23:43:15 +0600

    Print/Output

---
+++ Basic level/Output_print.java
@@ -0,0 +1,7 @@
+public class Output_print {
+    public static void main() {
+        System.err.println("Hi. I am Mohammad Tuhin");
+        System.out.println(251400052);
+        System.out.println(3 + 3);
+    }
+}
