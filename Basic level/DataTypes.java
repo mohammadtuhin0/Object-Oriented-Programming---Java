81cff424ad56f0ee8b937e21f977bbd2a4dd90dd
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-06-10 20:55:06 +0600

    Data types

---
+++ Basic level/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+
+    public static void main(String[] argv) {
+        byte num = 100;
+        System.out.println(num);
+
+        // The short data type can store whole numbers from -32768 to 32767:
+        short x = 5000;
+        System.err.println(x);
+
+        long y = 15000000000L;
+        System.err.println(y);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.err.println(isJavaFun);
+        System.err.println(isFishTasty);
+    }
+}
