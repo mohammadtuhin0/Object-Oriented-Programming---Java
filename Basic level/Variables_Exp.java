4645184659eba520b938a8d7bc5b6cbc8c0e245f
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-06-10 20:35:43 +0600

    Variables Examples

---
+++ Basic level/Variables_Exp.java
@@ -0,0 +1,19 @@
+public class Variables_Exp {
+
+    public static void main(String[] argv){
+        System.out.println("Real life Examples(Variables)");
+
+        // Students data
+        String name = "Don";
+        int studentID = 52;
+        int studentAge = 19;
+        int studentFee = 19000;
+        char studentGrade = 'B';
+
+        System.out.println("Student name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
+    }
+}
