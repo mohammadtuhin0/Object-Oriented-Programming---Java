68e57297342d98eeb3e5faf66453a6a25ed5a278
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-08-10 15:41:26 +0600

    task 4

--- Lab 6/UniversityTester.java
+++ Lab 6/UniversityTester.java
@@ -1,13 +1,19 @@
 public class UniversityTester {
     public static void main(String[] args) {
         University u1 = new University();
-        University u2 = u1;
+        University u2 = new University();
+
 
         System.out.println(u1);
 
         System.out.println("============");
-        
         System.out.println(u2);
+
+        if(u1 == u2){
+            System.out.println("Location is same");
+        } else{
+            System.out.println("Location is not same");
+        }
 
         u1.name = "Imperial College London";
         u1.country = "England";
@@ -17,5 +23,11 @@ public class UniversityTester {
 
         System.out.println(u2.name);
         System.out.println(u2.country);
+
+        if(u1.name == u2.name && u1.country == u2.country){
+            System.out.println("Same");
+        }else{
+            System.out.println("not same");
+        }
     }
 }
