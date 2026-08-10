8c32d0bae890b23ffd2705ae1a7bc39ee9675c26
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-08-10 12:26:56 +0600

    code update

--- Lab 5/Count_Frequency.java
+++ Lab 5/Count_Frequency.java
@@ -1,12 +1,9 @@
 import java.util.Scanner;
-
 public class Count_Frequency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter the element size");
         int n = sc.nextInt();
-
         int[] num = new int[n];
         System.out.println("Enter the "+ n + "size elements: ");
         for(int i = 0; i<n; i++){
